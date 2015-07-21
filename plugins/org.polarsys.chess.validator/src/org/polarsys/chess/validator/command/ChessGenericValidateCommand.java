c544851783cdcc8f3e007daa2647bab69dda03b5
Stefano Puri <stefano.puri@intecs.it>
2015-07-21 16:05:13 +0200

    [402] Add model validation command concerning CHESS constraints only. This is to avoid invocation of the additional constraints coming with UML; in fact the latters currently have issues regarding validation of base class for stereotypes (for a small project hundred of false errors are reported in the validation view). See CHESS BUG ID 402

--- plugins/org.polarsys.chess.validator/src/org/polarsys/chess/validator/command/ChessGenericValidateCommand.java
+++ plugins/org.polarsys.chess.validator/src/org/polarsys/chess/validator/command/ChessGenericValidateCommand.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Intecs.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Stefano Puri, stefano.puri@intecs.it
+ *******************************************************************************/
+
 package org.polarsys.chess.validator.command;
 
 import java.util.ArrayList;
