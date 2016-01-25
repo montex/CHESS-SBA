340113b1871f800d25a7923bad94ca45dd0a5403
Laura Baracchi <laura.baracchi@intecs.it>
2016-01-25 14:30:06 +0100

    Making NFP functions available
    Change-Id: I969b79b01c6e5ff818904abd7c7da38b41387d80
    Signed-off-by: Laura Baracchi <laura.baracchi@intecs.it>

---
+++ plugins/org.polarsys.chess.core/src/org/polarsys/chess/core/util/uml/ValueNFP.java
@@ -0,0 +1,17 @@
+/* ------------------------------------------------------------------------------
+ - Copyright (c) 2015 Intecs.
+ - All rights reserved. This program and the accompanying materials
+ - are made available under the terms of the Eclipse Public License v1.0
+ - which accompanies this distribution, and is available at
+ - http://www.eclipse.org/legal/epl-v10.html
+ -
+ - Contributors:
+ -    L. Baracchi, laura.baracchi@intecs.it
+ ------------------------------------------------------------------------------*/
+
+package org.polarsys.chess.core.util.uml;
+
+public class ValueNFP {
+	public String unit = new String();
+	public Double value = 0.0;
+}
