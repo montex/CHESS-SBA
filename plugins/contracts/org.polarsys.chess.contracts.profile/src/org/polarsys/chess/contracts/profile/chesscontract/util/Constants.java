e79dd51f90e9568d853f5439ab375beaa6eed923
Stefano Puri <stefano.puri@intecs.it>
2017-11-09 16:27:05 +0100

    added utility class related to stereotypes ID

---
+++ plugins/contracts/org.polarsys.chess.contracts.profile/src/org/polarsys/chess/contracts/profile/chesscontract/util/Constants.java
@@ -0,0 +1,5 @@
+package org.polarsys.chess.contracts.profile.chesscontract.util;
+
+public class Constants {
+	public static String ContractRefinementAnalysis = "CHESSContract::ContractRefinementAnalysisContext";
+}
