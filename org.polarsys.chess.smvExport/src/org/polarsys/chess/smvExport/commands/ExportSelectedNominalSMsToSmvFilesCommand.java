7f52072b78a8dc672b7551932f805f512f1edbe1
Alberto Debiasi <adebiasi@fbk.eu>
2018-02-02 14:09:00 +0100

    - added smvExport Plugin
    Change-Id: Iabf4f632af6cb46eba4824ccb280c0f2183eda95

---
+++ org.polarsys.chess.smvExport/src/org/polarsys/chess/smvExport/commands/ExportSelectedNominalSMsToSmvFilesCommand.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (C) 2017 Fondazione Bruno Kessler.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *   Alberto Debiasi - initial API and implementation
+ ******************************************************************************/
+package org.polarsys.chess.smvExport.commands;
+
+import java.util.Collection;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.uml2.uml.StateMachine;
+import org.polarsys.chess.diagram.ui.utils.ExportDialogUtils;
+import org.polarsys.chess.service.utils.SelectionUtil;
+import org.polarsys.chess.smvExport.services.SmvExportService;
+import eu.fbk.eclipse.standardtools.commands.AbstractJobCommand;
+import org.eclipse.uml2.uml.Class;
+
+public class ExportSelectedNominalSMsToSmvFilesCommand  extends AbstractJobCommand {
+
+	private SelectionUtil selectionUtil = SelectionUtil.getInstance();
+	private ExportDialogUtils exportDialogUtils = ExportDialogUtils.getInstance();
+	//private DirectoryUtil directoryUtils = DirectoryUtil.getInstance();
+	private SmvExportService smvExportService = SmvExportService.getInstance();
+	
+	public ExportSelectedNominalSMsToSmvFilesCommand() {
+		super("Export Nominal State Machines To .smv Files");
+	}
+
+	private Class umlSelectedComponent;	
+	private String outputDirectoryName;
+	private boolean showPopups;
+	//private String currentProjectName;
+	private Collection<StateMachine> stateMachines;
+	
+	@Override
+	public void execGUIOperations(ExecutionEvent event, IProgressMonitor monitor) throws Exception {
+		
+		umlSelectedComponent = selectionUtil.getUmlComponentFromSelectedObject(event);	
+		outputDirectoryName = exportDialogUtils.getDirectoryNameFromDialog();
+		//currentProjectName = directoryUtils.getCurrentProjectName();
+		showPopups = true;
+		stateMachines = smvExportService.getNominalStateMachines(umlSelectedComponent);
+	}
+
+	@Override
+	public void execJobCommand(ExecutionEvent event, IProgressMonitor monitor) throws Exception {
+		
+		smvExportService.exportNominalStateMachines(stateMachines,outputDirectoryName,showPopups,monitor);
+		
+	}
+
+}
---
+++ org.polarsys.chess.smvExport/src/org/polarsys/chess/smvExport/services/SmvExportService.java
@@ -0,0 +1,127 @@
+/*******************************************************************************
+ * Copyright (C) 2017 Fondazione Bruno Kessler.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *   Alberto Debiasi - initial API and implementation
+ ******************************************************************************/
+package org.polarsys.chess.smvExport.services;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.Set;
+
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.papyrus.uml.tools.model.UmlModel;
+import org.eclipse.papyrus.uml.tools.model.UmlUtils;
+import org.eclipse.uml2.uml.Class;
+import org.eclipse.uml2.uml.StateMachine;
+
+import org.polarsys.chess.contracts.profile.chesscontract.util.EntityUtil;
+import org.polarsys.chess.smvExport.model.UMLStateMachineModel;
+
+import eu.fbk.eclipse.standardtools.StateMachineTranslatorToSmv.services.SMVTranslatorService;
+import eu.fbk.eclipse.standardtools.exceptions.NoComponentException;
+import eu.fbk.eclipse.standardtools.utils.DialogUtil;
+
+public class SmvExportService {
+
+private static SmvExportService instance;
+	
+	//private final Logger logger = Logger.getLogger(SmvExportService.class);
+	private EntityUtil entityUtil = EntityUtil.getInstance();
+	//private StateMachineModelToSmvModelTranslator smTranslator = StateMachineModelToSmvModelTranslator.getInstance(UMLStateMachineModel.getInstance()); 
+	private SMVTranslatorService smvTranslatorService = SMVTranslatorService.getInstance(UMLStateMachineModel.getInstance());
+	private DialogUtil dialogUtil = DialogUtil.getInstance();
+	
+	public static SmvExportService getInstance(){
+		if(instance==null){
+			instance = new SmvExportService();
+		}
+		return instance;
+	}
+	
+	public void exportNominalStateMachines(Collection<StateMachine> stateMachines, String selectedDirectory,boolean showPopups,  IProgressMonitor monitor) throws Exception {
+		
+		for(StateMachine stateMachine : stateMachines){
+			smvTranslatorService.exportStateMachineToSmvFile(stateMachine,  selectedDirectory,null, monitor);
+		}
+		
+		if(showPopups){
+			dialogUtil.showMessage_ExportNominalStateMachinesDone(selectedDirectory);
+	}
+		
+	}
+	
+	public File exportSmv(Class umlSelectedComponent,boolean showPopups, String selectedDirectory, IProgressMonitor monitor)
+			throws Exception {
+		if (umlSelectedComponent == null) {
+			throw new NoComponentException();
+		}
+	
+		File smvFile = null;
+		
+		StateMachine stateMachine = getNominalStateMachine(umlSelectedComponent);
+		if(stateMachine!=null){
+		smvFile =  smvTranslatorService.exportStateMachineToSmvFile(stateMachine, selectedDirectory,getSmvFileName(umlSelectedComponent), monitor);	
+		if(showPopups){
+			dialogUtil.showMessage_ExportBehaviourDone(smvFile.getAbsolutePath());			
+	}
+		}else{			
+			dialogUtil.showMessage_NoStateMachine();
+		}
+		
+		return smvFile;
+		
+	}
+	
+	private StateMachine getNominalStateMachine(Class umlSelectedComponent) {		
+		Set<StateMachine> stateMachines = entityUtil.getNominalStateMachines(umlSelectedComponent);
+		if((stateMachines!=null)&&(stateMachines.size()>0)){
+		return stateMachines.iterator().next();
+		}else return null;
+	}
+
+	private String getSmvFileName(Class umlSelectedComponent) {
+		//	String systemQN = umlSelectedComponent.getQualifiedName();
+		//	String systemName = systemQN.substring(systemQN.lastIndexOf("::") + 2);
+		//	return umlSelectedComponent.getModel().getName() + "_" + systemName + ".smv";
+			return umlSelectedComponent.getName();
+		}
+	
+public Collection<StateMachine> getNominalStateMachines(Class umlSelectedComponent) {
+		
+	
+		Set<StateMachine> stateMachines =  entityUtil.getNominalStateMachines(umlSelectedComponent);
+		System.out.println("stateMachines");
+		for (StateMachine sm : stateMachines) {
+			System.out.println(sm);			
+		}
+		return stateMachines;
+	
+	}
+
+public Collection<StateMachine> getNominalStateMachines() {
+	
+	UmlModel umlModel = UmlUtils.getUmlModel();
+		Set<StateMachine> stateMachines =  entityUtil.getNominalStateMachines(umlModel);
+		System.out.println("stateMachines");
+		for (StateMachine sm : stateMachines) {
+			System.out.println(sm);			
+		}
+		return stateMachines;
+	
+	}
+
+
+
+
+
+
+
+	
+}
