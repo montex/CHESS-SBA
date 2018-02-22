b16110bfb733ff97e0af71efc8c042ddcfa9f06c
Alberto Debiasi <adebiasi@fbk.eu>
2018-02-22 10:53:25 +0100

    create first version of org.polarsys.chess.contracts.verificationService.test.runtime

---
+++ plugins/contracts/org.polarsys.chess.contracts.verificationService.test.runtime/src/org/polarsys/chess/contracts/verificationService/test/runtime/tests/AllTests.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Intecs.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Nicholas Pacini
+ *******************************************************************************/
+package org.polarsys.chess.contracts.verificationService.test.runtime.tests;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	//TestCommands.class,	
+	TestApplyRecursively.class, 
+	TestApplyCHESSStereotype.class, 
+	TestCreateViews.class,
+	TestModelChecking.class 
+	})
+public class AllTests {
+}
