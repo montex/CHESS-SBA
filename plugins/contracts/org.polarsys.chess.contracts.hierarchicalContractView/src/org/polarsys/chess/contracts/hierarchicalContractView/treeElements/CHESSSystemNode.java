fc5e29e90c658e3603744e562a131925e01fc085
Alberto Debiasi <adebiasi@fbk.eu>
2018-02-26 11:05:00 +0100

    small fix

--- plugins/contracts/org.polarsys.chess.contracts.hierarchicalContractView/src/org/polarsys/chess/contracts/hierarchicalContractView/treeElements/CHESSSystemNode.java
+++ plugins/contracts/org.polarsys.chess.contracts.hierarchicalContractView/src/org/polarsys/chess/contracts/hierarchicalContractView/treeElements/CHESSSystemNode.java
@@ -39,7 +39,7 @@ public class CHESSSystemNode extends SystemNode {
 	public void createChildren(Object system, List<TreeNode> children) {
 		for (Property umlProperty : ((Class) system).getAttributes()) {
 
-			if (contractEntityUtil.isContractProperty(umlProperty)) {
+			if (contractEntityUtil.isContractProperty(umlProperty)&&(entityUtil.getUmlType(umlProperty)!=null)) {
 				children.add(new ContractNode(this, ((Class) entityUtil.getUmlType(umlProperty)).getName()));
 			} else if (entityUtil.isComponentInstance(umlProperty)) {
 				children.add(new CHESSComponentNode(this, umlProperty));
