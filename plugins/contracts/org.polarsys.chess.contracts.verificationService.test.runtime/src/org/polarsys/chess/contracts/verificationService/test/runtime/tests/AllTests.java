e99cad5a303518cbfe559c8326b6162d05966c6d
Alberto Debiasi <adebiasi@fbk.eu>
2018-03-02 11:19:38 +0100

    added headers in verificationService.test.runtime

--- plugins/contracts/org.polarsys.chess.contracts.verificationService.test.runtime/src/org/polarsys/chess/contracts/verificationService/test/runtime/tests/AllTests.java
+++ plugins/contracts/org.polarsys.chess.contracts.verificationService.test.runtime/src/org/polarsys/chess/contracts/verificationService/test/runtime/tests/AllTests.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2015 Intecs.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     Nicholas Pacini
- *******************************************************************************/
 package org.polarsys.chess.contracts.verificationService.test.runtime.tests;
 
 
