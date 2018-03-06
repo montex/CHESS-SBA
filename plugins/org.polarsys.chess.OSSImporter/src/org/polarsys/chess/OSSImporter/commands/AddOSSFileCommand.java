9675a24f6f07bdb3b6ef3420424ef5c241965091
Luca Cristoforetti <cristofo@fbk.eu>
2018-03-06 11:27:14 +0100

    Some cleanup in OSSImporter

--- plugins/org.polarsys.chess.OSSImporter/src/org/polarsys/chess/OSSImporter/commands/AddOSSFileCommand.java
+++ plugins/org.polarsys.chess.OSSImporter/src/org/polarsys/chess/OSSImporter/commands/AddOSSFileCommand.java
@@ -15,42 +15,30 @@ import java.io.File;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.util.Diagnostician;
-import org.eclipse.emf.edit.ui.action.ValidateAction.EclipseResourcesUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.polarsys.chess.OSSImporter.actions.ImportOSSFileAction;
 import org.polarsys.chess.service.gui.utils.SelectionUtil;
 import org.eclipse.uml2.uml.Element;
 import org.eclipse.uml2.uml.Package;
-import org.eclipse.xtext.resource.XtextResource;
-import org.eclipse.xtext.resource.XtextResourceSet;
-import eu.fbk.eclipse.standardtools.utils.core.exceptions.NoResourceException;
 import eu.fbk.eclipse.standardtools.utils.ui.commands.AbstractJobCommand;
-import eu.fbk.eclipse.standardtools.utils.ui.utils.ErrorsDialogUtil;
+import eu.fbk.eclipse.standardtools.xtextService.ui.services.RuntimeErrorService;
 
 public class AddOSSFileCommand extends AbstractJobCommand implements IHandler {
 	private static final String SYSVIEW =	"CHESS::Core::CHESSViews::SystemView";
-	private static final String PROBLEMS_VIEW = "org.eclipse.ui.views.ProblemView";
 	private static final String DIALOG_TITLE =	"OSS parser";
 	
 	private ImportOSSFileAction sampleView;
 	private Object umlObject;
 	private File ossFile;
 	private Resource modelResource;
-	private boolean showNoErrorPopup = true;
-	private ErrorsDialogUtil ocraRuntimeErrorsDialogUtil = ErrorsDialogUtil.getInstance();
 	private SelectionUtil selectionUtil = SelectionUtil.getInstance();
 	
 	/**
@@ -69,100 +57,6 @@ public class AddOSSFileCommand extends AbstractJobCommand implements IHandler {
 		MessageDialog.openInformation(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),title, message);
 	}
 	
-	
-	//TODO try to reuse existing methods
-	private String[] showOSSRuntimeErrors(Resource resource, File file, boolean showNoErrorPopup, IProgressMonitor monitor) throws NoResourceException {
-		if (resource != null) {
-			if (file != null) {
-				if (file.exists()) {
-					monitor.subTask("show Runtime Errors");
-					String[] errors = showRuntimeErrors(resource, file);
-					ocraRuntimeErrorsDialogUtil.showMessage_RuntimeErrors(file.getName(), errors, showNoErrorPopup);
-					monitor.worked(1);
-					
-					return errors;
-				}
-			}
-		} else {
-			throw new NoResourceException();
-		}
-		return null;
-	}
-	
-	/**
-	 * Visualizes all the errors in the error view of Eclipse.
-	 */
-	private String[] showRuntimeErrors(Resource resource, File ossFile) {
-
-		XtextResource xTextRes = getXtextResourceFromFile(ossFile.getPath());
-
-		EclipseResourcesUtil eclipseResourcesUtil = new EclipseResourcesUtil();
-		eclipseResourcesUtil.deleteMarkers(resource);
-		Diagnostic diagnostic = Diagnostician.INSTANCE.validate(xTextRes.getContents().get(0));
-		if (diagnostic.getChildren().size() > 0) {
-
-			for (Diagnostic d : diagnostic.getChildren()) {
-				eclipseResourcesUtil.createMarkers(resource, d);
-			}
-
-			openProblemsView();
-
-			return getErrorsString(diagnostic);
-		}
-		return null;
-	}
-
-	private void openProblemsView() {
-		Display defaultDisplay = Display.getDefault();
-
-		defaultDisplay.syncExec(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(PROBLEMS_VIEW);
-				} catch (PartInitException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	private String[] getErrorsString(Diagnostic diagnostic) {
-		String[] errors = new String[diagnostic.getChildren().size()];
-		int i = 0;
-		for (Diagnostic d : diagnostic.getChildren()) {
-			errors[i] = d.getMessage();
-			i++;
-		}
-		return errors;
-	}
-
-	/**
-	 * @param path
-	 *            the path of the oss file
-	 * @return the XTextResource of the oss model
-	 */
-	private XtextResource getXtextResourceFromFile(String path) {
-		final XtextResourceSet resourceSet = new XtextResourceSet();
-		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
-		return (XtextResource) resourceSet.getResource(URI.createFileURI(path), true);
-	}
-	
-	/**
-	 * Checks if the given OSS file is valid or contains errors
-	 */
-	private String[] showRuntimeErrors(Resource resource, File ossFile, boolean showNoErrorPopup, IProgressMonitor monitor) throws Exception {
-
-		monitor.beginTask("Show Runtime Errors", 1);
-		
-		String[] errors = showOSSRuntimeErrors(resource, ossFile,showNoErrorPopup, monitor);
-
-		monitor.done();
-		
-		return errors;
-	}
-
-	//TODO this method already exist in another plugin 
 	/**
 	 * Displays a file dialog to select the OSS file
 	 * @return the selected File
@@ -182,7 +76,6 @@ public class AddOSSFileCommand extends AbstractJobCommand implements IHandler {
 		}
 		return ossFile;
 	}
-	
 	
 	/**
 	 * Checks if the selected object is a package in the <<SystemView>> branch 
@@ -213,18 +106,17 @@ public class AddOSSFileCommand extends AbstractJobCommand implements IHandler {
 		umlObject = selectionUtil.getUmlSelectedObject(selection);
 		modelResource = selectionUtil.getSelectedModelResource();
 	
-//		System.out.println("selectedUmlElement: " + umlObject);
-		
 		if (isSystemViewPackage((Element) umlObject)) {
 			ossFile = getOSSFile();
 
-			final String[] errors = showRuntimeErrors(modelResource, ossFile, showNoErrorPopup, monitor);
+			// Check if there are errors in the OSS file
+			final boolean isValid = RuntimeErrorService.getInstance().showOSSRuntimeErrors(ossFile, modelResource, true, true, monitor);
 			
 			monitor.beginTask("Importing elements from OSS file", 1);
 
 			sampleView = ImportOSSFileAction.getInstance();
 
-			if (errors == null && sampleView != null) {
+			if (isValid && sampleView != null) {
 				IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
 				activePage.setEditorAreaVisible(false);
 				try {
@@ -245,15 +137,5 @@ public class AddOSSFileCommand extends AbstractJobCommand implements IHandler {
 
 	@Override
 	public void execJobCommand(ExecutionEvent event, IProgressMonitor monitor) throws Exception {
-		
-//		String[] errors = showRuntimeErrors(modelResource, ossFile, showNoErrorPopup, monitor);
-//		
-//		monitor.beginTask("Importing elements from OSS file", 1);
-//		
-//		if (errors == null && sampleView != null) {
-//			sampleView.startParsing((Package) umlObject, ossFile);
-//		}
-//		
-//		monitor.done();
 	}
 }
