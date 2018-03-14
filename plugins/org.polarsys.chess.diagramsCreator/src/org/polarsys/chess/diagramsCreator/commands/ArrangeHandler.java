75f226f5ce5af0aef870d5c5d0494365353201c9
Luca Cristoforetti <cristofo@fbk.eu>
2018-03-14 18:10:43 +0100

    Improvements in IBD creation, not finished yet.

--- plugins/org.polarsys.chess.diagramsCreator/src/org/polarsys/chess/diagramsCreator/commands/ArrangeHandler.java
+++ plugins/org.polarsys.chess.diagramsCreator/src/org/polarsys/chess/diagramsCreator/commands/ArrangeHandler.java
@@ -18,7 +18,6 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.requests.ArrangeRequest;
-import org.eclipse.papyrus.sysml.diagram.common.edit.part.BlockPropertyCompositeEditPart;
 import org.polarsys.chess.service.gui.utils.SelectionUtil;
 
 public class ArrangeHandler extends AbstractHandler {
@@ -29,44 +28,25 @@ public class ArrangeHandler extends AbstractHandler {
 		final IGraphicalEditPart selectedEditPart = SelectionUtil.getInstance().getSelectedGraphicalObject(event);
 
 		// Get the EditPart of the main element
-		IGraphicalEditPart elementEditPart = (IGraphicalEditPart) selectedEditPart.getChildren().get(0); 
-		System.out.println("processing child = " + elementEditPart);
+		final IGraphicalEditPart elementEditPart = (IGraphicalEditPart) selectedEditPart.getChildren().get(0); 
 		
 		// Read the parameter to understand what to arrange
 		final String processDiagram = event.getParameter(CreateBDDCommand.ARRANGE_PROCESS_DIAGRAM);
 		
-		System.out.println("processDiagram = " + processDiagram);
-		
 		if (processDiagram != null && processDiagram.equals("false")) {
 			
-			
-			List<?> children = elementEditPart.getChildren();
-			for (Object object : children) {
-				System.out.println("child = " + object);
-			}
-			
-			// Process the child of the main element
+			// Process the structure compartment of the main element
 			selection.add((IGraphicalEditPart) elementEditPart.getChildren().get(1));
-			List<?> selectionChildren = selection.get(0).getChildren();
-			for (Object object : selectionChildren) {
-				System.out.println("selchil = " + object);
-				if (object instanceof BlockPropertyCompositeEditPart) {
-					selection.add((IGraphicalEditPart) object);
-				}
-			}
-					
-			System.out.println("processing second child = " + selection.get(0));
 		} else {
 
 			// Process the main element
-			selection.add(elementEditPart);
+			selection.add(selectedEditPart);
 		}
 		
 		final ArrangeRequest request = new ArrangeRequest("arrangeAllAction"); //$NON-NLS-1$
-//		final ArrangeRequest request = new ArrangeRequest("arrangeSelectionAction"); //$NON-NLS-1$
 		request.setPartsToArrange((selection));
 
-		final Command cmd = selectedEditPart.getCommand(request);
+		final Command cmd = selection.get(0).getCommand(request);
 
 		// Retrieve the number of times to execute the command
 		int loopTimes = 1;
