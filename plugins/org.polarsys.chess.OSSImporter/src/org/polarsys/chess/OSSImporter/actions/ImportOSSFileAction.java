24f1c6ee09191f3ad09ea643d0bab88ca91ca0fd
Luca Cristoforetti <cristofo@fbk.eu>
2018-04-16 09:17:22 +0200

    Added some error messages in case of unknown OSS tags.

--- plugins/org.polarsys.chess.OSSImporter/src/org/polarsys/chess/OSSImporter/actions/ImportOSSFileAction.java
+++ plugins/org.polarsys.chess.OSSImporter/src/org/polarsys/chess/OSSImporter/actions/ImportOSSFileAction.java
@@ -465,6 +465,10 @@ public class ImportOSSFileAction {
 					final String message = "Found a PROP tag, don't know how to handle it!";
 					logger.error("Import Error: " + message);
 					importErrors.append(message + "\n");
+				} else if (dslRefInstance != null) {
+					final String message = "Found a " + dslRefInstance + " tag, don't know how to handle it!";
+					logger.error("Import Error: " + message);
+					importErrors.append(message + "\n");
 				}
 			}
 		}
@@ -729,6 +733,10 @@ public class ImportOSSFileAction {
 						final String message = "Found a OPERATION tag, don't know how to handle it!";
 						logger.error("Import Error: " + message);
 						importErrors.append(message + "\n");
+					} else {
+						final String message = "Found a " + dslVariable + " tag, don't know how to handle it!";
+						logger.error("Import Error: " + message);
+						importErrors.append(message + "\n");						
 					}
 				} else if (dslIntInstance != null && dslIntInstance.getDefine() != null) {
 				
@@ -791,6 +799,10 @@ public class ImportOSSFileAction {
 						final ContractProperty contractProperty = (ContractProperty) chessSystemModel.getContract(owner, dslContract.getName());
 						mapContractProperties.put(contractProperty.getBase_Property().getQualifiedName(), Boolean.TRUE);
 					}
+				} else if (dslIntInstance != null) {
+					final String message = "Found a " + dslIntInstance + " tag, don't know how to handle it!";
+					logger.error("Import Error: " + message);
+					importErrors.append(message + "\n");						
 				}
 			}
 		}
