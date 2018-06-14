c596442d4f5b7adf6ca7eae4dca877dd0e68f7ac
Alberto Debiasi <adebiasi@fbk.eu>
2018-06-14 15:48:35 +0200

    fixed ConstraintEditor to support only Constraints that are DelegationConstraints
    Change-Id: I147c0e145b8ca1e25fe91f635e58adf4f11f037d

--- plugins/org.polarsys.chess.constraints.constraintEditor/src/org/polarsys/chess/constraints/constraintEditor/ConstraintEditorFilter.java
+++ plugins/org.polarsys.chess.constraints.constraintEditor/src/org/polarsys/chess/constraints/constraintEditor/ConstraintEditorFilter.java
@@ -28,7 +28,7 @@ public class ConstraintEditorFilter implements org.eclipse.jface.viewers.IFilter
 		Object selectedUmlElement = selectionUtil.getUmlSelectedObject(selected);
 
 		if (selectedUmlElement instanceof Constraint) {
-			if (!entityUtil.isFormalProperty((Constraint) selectedUmlElement)) {
+			if (entityUtil.isDelegationConstraints((Constraint) selectedUmlElement)) {
 				return true;
 			}
 		}
