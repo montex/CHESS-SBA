714a3f34b8c9d7c34a629ecf361baf8c74918798
Alberto Debiasi <adebiasi@fbk.eu>
2018-08-10 11:21:06 +0200

    updated the set contract refinement command
    Change-Id: Ib664ebd55f41ee7e0796b58cbe8cce5e7f2027fe

--- plugins/contracts/org.polarsys.chess.contracts.chessextension/src/org/polarsys/chess/contracts/chessextension/popup/actions/SetContractRefinement.java
+++ plugins/contracts/org.polarsys.chess.contracts.chessextension/src/org/polarsys/chess/contracts/chessextension/popup/actions/SetContractRefinement.java
@@ -18,11 +18,13 @@ package org.polarsys.chess.contracts.chessextension.popup.actions;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.gmf.runtime.notation.Node;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.window.Window;
+import org.eclipse.papyrus.emf.facet.custom.metamodel.v0_2_0.internal.treeproxy.impl.EObjectTreeElementImpl;
 import org.eclipse.papyrus.sysml.diagram.common.edit.part.ConstraintPropertyChildLabelEditPart;
 import org.eclipse.papyrus.uml.diagram.clazz.edit.parts.PropertyForClassEditPart;
 import org.eclipse.papyrus.uml.diagram.clazz.edit.parts.PropertyForComponentEditPart;
@@ -41,6 +43,7 @@ import org.polarsys.chess.contracts.chessextension.managers.CHESSContractProfile
 import org.polarsys.chess.contracts.profile.chesscontract.ContractProperty;
 import org.polarsys.chess.contracts.profile.chesscontract.ContractRefinement;
 
+import eu.fbk.eclipse.standardtools.utils.ui.utils.DialogUtil;
 
 public class SetContractRefinement implements IObjectActionDelegate {
 
@@ -48,10 +51,8 @@ public class SetContractRefinement implements IObjectActionDelegate {
 	protected static final String CONTRACT_REFINEMENt = "CHESSContract::ContractRefinement";
 	public static final String COMPONENT_INSTANCE = "CHESSContract::ComponentInstance";
 	private Shell shell;
-	private ConstraintPropertyChildLabelEditPart sysmlEditPart;
-	private PropertyForClassEditPart umlClassEditPart;
-	private PropertyForComponentEditPart umlCompEditPart;
 	private TransactionalEditingDomain editdomain;
+	private Property contrProp;
 
 	public SetContractRefinement() {
 		super();
@@ -62,87 +63,106 @@ public class SetContractRefinement implements IObjectActionDelegate {
 	}
 
 	public void run(IAction action) {
+		try {
+			setSelection();
 
-		setSelection();
-		Property contrProp = null;
-		if(sysmlEditPart != null){
-			Node notationNode = (Node) sysmlEditPart.getModel();
-			contrProp = (Property) notationNode.getElement();
-			editdomain = sysmlEditPart.getEditingDomain();
-		}else if(umlClassEditPart != null){
-			Node notationNode = (Node) umlClassEditPart.getModel();
-			contrProp = (Property) notationNode.getElement();
-			editdomain = umlClassEditPart.getEditingDomain();
-		}else if (umlCompEditPart != null) {
-			Node notationNode = (Node) umlCompEditPart.getModel();
-			contrProp = (Property) notationNode.getElement();
-			editdomain = umlCompEditPart.getEditingDomain();
-		}
-		if(contrProp != null){
-			Stereotype contrPropStereo = contrProp.getAppliedStereotype(CONTRACT_PROPERTY);
-			Object temp = contrProp.getValue(contrPropStereo, "RefinedBy");
-			if(temp instanceof EList<?>){
-				@SuppressWarnings("unchecked")
-				final EList<ContractRefinement> refineList = (EList<ContractRefinement>) temp;
-				final Class ownerClass = (Class) contrProp.getOwner();
-				final SetContractRefinementDialog dialog = new SetContractRefinementDialog(shell, ownerClass);
-				dialog.create();
-				if (dialog.open() == Window.OK) {
-					if(dialog.getSelected() != null){
-						final String[] selection = dialog.getSelected();
-						editdomain.getCommandStack().execute(
-								new RecordingCommand(editdomain) {
-									protected void doExecute() {
-										for (int i = 0; i < selection.length; i++) {
-											String stringRef = selection[i];
-											Classifier present = ownerClass.getNestedClassifier(stringRef);
-											if(present == null){
-												DataType dt = UMLFactory.eINSTANCE.createDataType();
-												ownerClass.getNestedClassifiers().add(dt);
-												dt.setName(stringRef);
-												Stereotype st = CHESSContractProfileManager.getContractRefinement();
-												dt.applyStereotype(st);
-												String partName = stringRef.substring(0, stringRef.indexOf("."));
-												String contrName = stringRef.substring(stringRef.indexOf(".") + 1, stringRef.length());
-												ContractRefinement contRef = (ContractRefinement) dt.getStereotypeApplication(st);
-												Property instance = ownerClass.getPart(partName, null);
-												contRef.setInstance(instance);
-												Property contrProp = ((Classifier) instance.getType()).getAttribute(contrName, null);
-												ContractProperty contract = (ContractProperty) contrProp.getStereotypeApplication(contrProp.getAppliedStereotype(CONTRACT_PROPERTY));
-												contRef.setContract(contract);
-												refineList.add(contRef);
-											}else{
-												ContractRefinement cr = (ContractRefinement) present.getStereotypeApplication(present.getAppliedStereotype(CONTRACT_REFINEMENt));
-												refineList.add(cr);
-											}
-										}
-									}
-								});
-					} 
+			if (contrProp != null) {
+				Stereotype contrPropStereo = contrProp.getAppliedStereotype(CONTRACT_PROPERTY);
+				if (contrPropStereo == null) {
+					throw new Exception("Select the ContractProperty element to set the contract refinement");
 				}
+				Object temp = contrProp.getValue(contrPropStereo, "RefinedBy");
+				if (temp instanceof EList<?>) {
+					@SuppressWarnings("unchecked")
+					final EList<ContractRefinement> refineList = (EList<ContractRefinement>) temp;
+					final Class ownerClass = (Class) contrProp.getOwner();
+					final SetContractRefinementDialog dialog = new SetContractRefinementDialog(shell, ownerClass);
+					dialog.populateRefineListAndCreateDialog();
+					if (dialog.open() == Window.OK) {
+						if ((dialog.getSelected() != null)) {
 
-			}else{
-				//do nothing
+							editdomain.getCommandStack().execute(new RecordingCommand(editdomain) {
+								protected void doExecute() {
+									for (SetContractRefinementDialog.ContractRefinementObj currContractRefinementObj : dialog
+											.getSelected()) {
+
+										Classifier existingContractRefinement = ownerClass
+												.getNestedClassifier(currContractRefinementObj.getID());
+										if (existingContractRefinement == null) {
+											DataType dt = UMLFactory.eINSTANCE.createDataType();
+											ownerClass.getNestedClassifiers().add(dt);
+											dt.setName(currContractRefinementObj.getID());
+											Stereotype st = CHESSContractProfileManager.getContractRefinement();
+											dt.applyStereotype(st);
+											String partName = currContractRefinementObj.getSubComponentName();
+											String contrName = currContractRefinementObj.getContractName();
+											ContractRefinement contRef = (ContractRefinement) dt
+													.getStereotypeApplication(st);
+											Property instance = ownerClass.getPart(partName, null);
+											contRef.setInstance(instance);
+											Property contrProp = ((Classifier) instance.getType())
+													.getAttribute(contrName, null);
+											ContractProperty contract = (ContractProperty) contrProp
+													.getStereotypeApplication(
+															contrProp.getAppliedStereotype(CONTRACT_PROPERTY));
+											contRef.setContract(contract);
+											contRef.setLowerIndexOfInstance(currContractRefinementObj.getLower());
+											contRef.setUpperIndexOfInstance(currContractRefinementObj.getUpper());
+											refineList.add(contRef);
+										} else {
+											ContractRefinement cr = (ContractRefinement) existingContractRefinement
+													.getStereotypeApplication(existingContractRefinement
+															.getAppliedStereotype(CONTRACT_REFINEMENt));
+											refineList.add(cr);
+										}
+
+									}
+								}
+							});
+						}
+					}
+
+				} else {
+					// do nothing
+				}
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			DialogUtil.getInstance().showMessage_ExceptionError(e);
 		}
 	}
 
+	private void setSelection() throws Exception {
 
-	private void setSelection() {
-
-		ISelection selection = PlatformUI.getWorkbench()
-				.getActiveWorkbenchWindow().getSelectionService()
+		ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService()
 				.getSelection();
 		if (selection instanceof IStructuredSelection) {
 			IStructuredSelection strucSelection = (IStructuredSelection) selection;
 			if (strucSelection.size() == 1) {
 				Object partSelected = strucSelection.iterator().next();
-				if(partSelected instanceof ConstraintPropertyChildLabelEditPart){
-					sysmlEditPart = (ConstraintPropertyChildLabelEditPart) partSelected;
-				}else if(partSelected instanceof PropertyForClassEditPart){
-					umlClassEditPart = (PropertyForClassEditPart) partSelected;
-				}else if(partSelected instanceof PropertyForComponentEditPart){
-					umlCompEditPart = (PropertyForComponentEditPart) partSelected;
+				if (partSelected instanceof ConstraintPropertyChildLabelEditPart) {
+					ConstraintPropertyChildLabelEditPart sysmlEditPart = (ConstraintPropertyChildLabelEditPart) partSelected;
+					Node notationNode = (Node) sysmlEditPart.getModel();
+					contrProp = (Property) notationNode.getElement();
+					editdomain = sysmlEditPart.getEditingDomain();
+				} else if (partSelected instanceof PropertyForClassEditPart) {
+					PropertyForClassEditPart umlClassEditPart = (PropertyForClassEditPart) partSelected;
+					Node notationNode = (Node) umlClassEditPart.getModel();
+					contrProp = (Property) notationNode.getElement();
+					editdomain = umlClassEditPart.getEditingDomain();
+				} else if (partSelected instanceof PropertyForComponentEditPart) {
+					PropertyForComponentEditPart umlCompEditPart = (PropertyForComponentEditPart) partSelected;
+					Node notationNode = (Node) umlCompEditPart.getModel();
+					contrProp = (Property) notationNode.getElement();
+					editdomain = umlCompEditPart.getEditingDomain();
+				} else if (partSelected instanceof EObjectTreeElementImpl) {
+					EObjectTreeElementImpl eObjectTreeElementImpl = (EObjectTreeElementImpl) partSelected;
+					System.out.println("--> " + eObjectTreeElementImpl.getEObject());
+					if (!(eObjectTreeElementImpl.getEObject() instanceof Property)) {
+						throw new Exception("Select the ContractProperty element to set the contract refinement");
+					}
+					contrProp = (Property) eObjectTreeElementImpl.getEObject();
+					editdomain = TransactionUtil.getEditingDomain(contrProp);
 				}
 			}
 		}
